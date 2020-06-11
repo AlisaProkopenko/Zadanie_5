4560e180057dbd405abc389508969b8bda447a9c
AlisaProkopenko <66774998+AlisaProkopenko@users.noreply.github.com>
2020-06-11 17:29:41 +0300

    Add files via upload

---
+++ zadanie_5.js
@@ -0,0 +1,6 @@
+document.write("Задание 2", "<br>");
+document.write("Прокопенко Алиса", "<br>");
+document.write(5+6,"<br>");
+document.write(4-5,"<br>");
+document.write(3*4+2,"<br>");
+document.write(4/7,"<br>");
